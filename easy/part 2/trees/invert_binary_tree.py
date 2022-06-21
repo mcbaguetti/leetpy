d237e98af92ba0f440f963092fce507c15f25aff
sp203597 <comelli.tom@gmail.com>
2022-06-21 22:36:54 +0200

    first tree problem

---
+++ easy/part 2/trees/invert_binary_tree.py
@@ -0,0 +1,36 @@
+"""
+226. Invert Binary Tree
+Easy
+
+Given the root of a binary tree, invert the tree, and return its root.
+
+Example 1:
+Input: root = [4,2,7,1,3,6,9]
+Output: [4,7,2,9,6,3,1]
+
+Example 2:
+Input: root = [2,1,3]
+Output: [2,3,1]
+
+Example 3:
+Input: root = []
+Output: []
+
+
+Constraints:
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+"""
+
+
+def invert_bin_tree(self, root):
+
+    if not root:
+        return None
+
+    root.left, root.right = root.right, root.left
+
+    self.invert_bin_tree(root.left)
+    self.invert_bin_tree(root.right)
+
+    return root
