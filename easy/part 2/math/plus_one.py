a8edc3686734d5cb308ea549ddd2ca02b5113aaf
T. C <37833294+mcbaguetti@users.noreply.github.com>
2022-09-23 15:09:49 +0200

    Create plus_one.py

---
+++ easy/part 2/math/plus_one.py
@@ -0,0 +1,21 @@
+def plus_one(digits):
+        carry, i = 1, 0
+
+        digits = digits[::-1]
+
+        while carry:
+                if i < len(digits):
+                        if digits[i] == 9:
+                                digits[i] = 0
+
+                        else:
+                                digits[i] += 1
+                                carry = 0
+                ## 9 -> 10
+                else:
+                       digits.append(1)
+                       carry = 0
+
+                i += 1
+
+        return digits[::-1]
