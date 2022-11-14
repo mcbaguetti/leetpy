69516d2d2e3cabc8b85e9dc53204f25e6f311d72
T. C <37833294+mcbaguetti@users.noreply.github.com>
2022-11-14 16:16:05 +0100

    Create rotated_array.py

---
+++ medium/part 1/binary_search/rotated_array.py
@@ -0,0 +1,63 @@
+"""
+33. Search in Rotated Sorted Array
+Medium
+
+There is an integer array nums sorted in ascending order (with distinct values).
+
+Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such
+that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed).
+For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums,
+or -1 if it is not in nums.
+You must write an algorithm with O(log n) runtime complexity.
+
+Example 1:
+Input: nums = [4,5,6,7,0,1,2], target = 0
+Output: 4
+
+Example 2:
+Input: nums = [4,5,6,7,0,1,2], target = 3
+Output: -1
+
+Example 3:
+Input: nums = [1], target = 0
+Output: -1
+"""
+
+
+def search_sorted_rotated(nums, target):
+    l, r = 0, len(nums) - 1
+    mid = 0
+
+    if len(nums) == 1 and nums[l] == target:
+        return l
+
+    if (nums[l] > nums[r]):
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] > nums[r]:
+                l = mid + 1
+            elif nums[mid] < nums[r]:
+                r = mid
+            else:
+                break
+
+        l, r = 0, len(nums) - 1
+        if nums[mid] <= target <= nums[r]:
+            l = mid
+        else:
+            r = mid - 1
+
+    while l <= r:
+        mid = (l + r) // 2
+        if nums[mid] > target:
+            r = mid - 1
+        elif nums[mid] < target:
+            l = mid + 1
+        else:
+            return mid
+
+    return -1
+
+print(search_sorted_rotated([4,5,6,7,0,1,2], 0))
+
